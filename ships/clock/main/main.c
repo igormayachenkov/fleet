2087210fac78bb43b4ffdac011cadf43a07ddfd7
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2024-12-16 18:07:37 +0300

    clock: segments added

--- ships/clock/main/main.c
+++ ships/clock/main/main.c
@@ -9,14 +9,46 @@
 static const char *TAG = "clock";
 
 // CHANNELS LIST
-static gpio_num_t channels[4] = {13,12,14,27};
+#define CHANNELS_N 4
+//static gpio_num_t channels[CHANNELS_N] = {13,12,14,27};
+// static gpio_num_t channels[CHANNELS_N] = {26,25,33,32}; works   
+static gpio_num_t channels[CHANNELS_N] = {18,19,21,3};// works
+//static gpio_num_t channels[CHANNELS_N] = {3,3,22,23};// works
 static uint8_t    channel = 0; // current channel
-
 #define CHANNEL_ON  0
 #define CHANNEL_OFF 1
 
+// SEGMENTS LIST
+#define SEGMENTS_N 7
+static gpio_num_t segments[SEGMENTS_N] = {36,36,36,36,36,36,36};
+
+// SYMBOLS in 7_seg indicator
+//   a
+// f   b
+//   g
+// e   c
+//   d    [dp]
+#define SYMBOLS_N 10
+static uint8_t symbols[SYMBOLS_N][SEGMENTS_N] = {
+    //   a,b,c,d,e,f,g
+        {1,1,1,1,1,1,0}, // 0
+        {0,1,1,0,0,0,0}, // 1
+        {1,1,0,1,1,0,1}, // 2
+        {1,1,1,1,0,0,1}, // 3
+        {0,1,1,0,0,1,1}, // 4
+        {1,0,1,1,0,1,1}, // 5
+        {1,0,1,1,1,1,1}, // 6
+        {1,1,1,0,0,0,0}, // 7
+        {1,1,1,1,1,1,1}, // 8
+        {1,1,1,1,0,1,1}  // 9
+    };
+
+// VIDEO MEMORY (symbols for channels)
+static uint8_t videoMemory[CHANNELS_N] = {1,2,3,4};
+
 void app_main(void)
 {
+    uint8_t i;
     int pt = portTICK_PERIOD_MS;
     ESP_LOGI(TAG, "***** APP STARTED *****" );
     ESP_LOGI(TAG, "portTICK_PERIOD_MS: %u", pt);
@@ -29,7 +61,9 @@ void app_main(void)
     //set as output mode
     io_conf.mode = GPIO_MODE_OUTPUT;
     //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = ((1ULL<<channels[0]) | (1ULL<<channels[1]) | (1ULL<<channels[2]) | (1ULL<<channels[3]));
+    io_conf.pin_bit_mask = 0;
+    for( i=0; i<CHANNELS_N; i++ ) io_conf.pin_bit_mask |= (1ULL<<channels[i]);
+//    for( i=0; i<SEGMENTS_N; i++ ) io_conf.pin_bit_mask |= (1ULL<<segments[i]);
     //disable pull-down mode
     io_conf.pull_down_en = 0;
     //disable pull-up mode
@@ -44,7 +78,12 @@ void app_main(void)
 
         // Select new channel
         channel++;
-        if(channel==4) channel=0;
+        if(channel==CHANNELS_N) channel=0;
+
+        // Update pins according to "video memory" (segments for the channel)
+        // uint8_t* symbol = symbols[videoMemory[channel]]; // Get current symbol for the channel
+        // for(i=0; i<SEGMENTS_N; i++)
+        //     gpio_set_level(segments[i], symbol[i]); // Set segment value
 
         // Show new channel
         gpio_set_level(channels[channel], CHANNEL_ON);
